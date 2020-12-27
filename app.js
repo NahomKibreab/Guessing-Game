e70fde896e069a874411fc39cd7cb8307b1f6c7e
NahomKibreab <nknahom@gmail.com>
2020-12-26 21:04:56 -0700

    Guess the number

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let maximumNumber = parseInt(prompt('Enter the maximum number!'));
+while (!maximumNumber) {
+  maximumNumber = parseInt(prompt('Enter a valid Number'));
+}
+
 let randomNumber = Math.floor(Math.random() * maximumNumber) + 1;
 let guessNumber = parseInt(prompt('Guess the number'));
 let attempts = 1;
 
-while (!guessNumber) {
-  guessNumber = parseInt(prompt('Invalid Number'));
-}
-
 while (parseInt(guessNumber) !== randomNumber) {
   attempts++;
-  guessNumber = prompt('Guess the number');
+  if (guessNumber > randomNumber) {
+    guessNumber = prompt('Too high! Enter a new guess:');
+  } else {
+    guessNumber = prompt('Too low! Enter a new guess');
+  }
   if (guessNumber === 'q') break;
 }
 
